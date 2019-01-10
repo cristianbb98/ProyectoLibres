98643a1f32d33ac17704fd3b383453339455d793
cristianbb98 <cristianbb98@gmail.com>
2019-01-10 15:42:24 -0500

    Arreglo interfaz foro

--- sgoa/aplicacion/modulos_comunes/modulo_foro/formulario.php
+++ sgoa/aplicacion/modulos_comunes/modulo_foro/formulario.php
@@ -1,4 +1,15 @@
 <?php
+
+session_start();
+if (@!$_SESSION['usuario']) {
+    header("Location:../../index.php");
+} elseif ($_SESSION['tipo_usuario'] == 'EST') {
+//header("Location:index2.php");
+    echo "eres estudiante";
+} elseif ($_SESSION['tipo_usuario'] == 'ADM') {
+    echo "eres administrador";
+}
+
 	if(isset($_GET["respuestas"]))
 		$respuestas = $_GET['respuestas'];
 	else
@@ -7,18 +18,112 @@
 		$identificador = $_GET['identificador'];
 	else
     $identificador = 0;
-  if(isset($_GET["nombre"]))
-		$nombre = $_GET['nombre'];
-	else
-    $nombre = 'NULL';
-  if(isset($_GET["idLogin"]))
-		$idLogin = $_GET['idLogin'];
-	else
-    $idLogin = 'NULL';
-    
+
+    $idLogin = $_SESSION['id'];
+
+    $nombre = $_SESSION['usuario'];
     
 ?>
-<table>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="es">
+<head>
+
+    <meta charset="utf-8"></meta>
+    <link rel="stylesheet" href="../../../plugins/bootstrap/css/bootstrap.min.css"></link>
+    <script type="text/javascript" src="../../../plugins/bootstrap/js/jquery-3.3.1.js"></script>
+    <script type="text/javascript" src="../../../plugins/bootstrap/js/bootstrap.min.js"></script>
+    <title>Proyecto SGOA</title>
+</head>
+<style>
+    /* Remove the navbar's default margin-bottom and rounded borders */
+    .navbar {
+        margin-bottom: 0;
+        border-radius: 0;
+    }
+
+    /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
+    .row.content {height: 390px}
+
+    /* Set gray background color and 100% height */
+    .sidenav {
+        padding-top: 20px;
+        background-color: #f1f1f1;
+        height: 100%;
+    }
+
+    html{
+        min-height: 100%;
+        position: relative;
+    }
+    body{
+        margin:0;
+        margin-bottom: 40px;
+    }
+    /* Set black background color, white text and some padding */
+    footer {
+        background-color: #555;
+        color: white;
+        padding: 15px;
+        position: fixed;
+        bottom: 0;
+        width: 100%;
+        padding-top:5px;
+    padding-bottom:5px;
+    }
+
+    /* On small screens, set height to 'auto' for sidenav and grid */
+    @media screen and (max-width: 767px) {
+        .sidenav {
+            height: auto;
+            padding: 15px;
+        }
+        .row.content {height:auto;}
+    }
+
+    .table > tbody > tr > td {
+        vertical-align: middle;
+    }
+
+       .estadistica{
+        -webkit-column-count: 3; /* Chrome, Safari, Opera */
+        -moz-column-count: 3; /* Firefox */
+        column-count: 2;
+        
+    }
+
+</style>
+
+
+<body>
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Bienvenid@: <strong><?php echo $_SESSION['usuario'] ?></strong></a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+                <li><a href="../../modulos_profesor/pro_importar_catalogar.php">Importar y catalogar</a></li>
+                <li><a href="../../modulos_profesor/pro_herramientas.php">Herramientas</a></li>
+                <li class="active"><a href="../../modulos_comunes/index.php?<?php echo $_SESSION['usuario']?>">Foro</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="../../desconectar_sesion.php"><span class="glyphicon glyphicon-log-out"></span> Salir</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<!-- Inicio formulario de búsqueda -->
+
+<div class="container-fluid text-center">
+    <div class="row content">
+       
+      <table>
 <form name="form" action="agregar.php" method="post">
 	<input type="hidden" name="identificador" value="<?php echo $identificador;?>">
 	<input type="hidden" name="respuestas" value="<?php echo $respuestas;?>">
@@ -40,6 +145,10 @@
     </tr>
     </form>
     
-    <td><a href='index.php?nombre=<?php echo $nombre;?>'>Regresar</a></td>
+    <td><a href='index.php'>Regresar</a></td>
     
 </table>
+</div>
+      </div>
+</body>
+</html>
--- sgoa/aplicacion/modulos_comunes/modulo_foro/index.php
+++ sgoa/aplicacion/modulos_comunes/modulo_foro/index.php
@@ -1,11 +1,208 @@
-<table width="1300px">
-	<tr>
-		<td width="300px">Usuario</td>
-		<td width="300px">Título</td>
-		<td width="300px">Fecha</td>
-		<td width="300px">Número de respuestas</td>
-		<td width="100px"></td>
-	</tr>
+<?php
+
+session_start();
+if (@!$_SESSION['usuario']) {
+    header("Location:../../index.php");
+} elseif ($_SESSION['tipo_usuario'] == 'EST') {
+//header("Location:index2.php");
+    echo "eres estudiante";
+} elseif ($_SESSION['tipo_usuario'] == 'ADM') {
+    echo "eres administrador";
+}
+   
+    
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="es">
+<head>
+
+    <meta charset="utf-8"></meta>
+    <link rel="stylesheet" href="../../../plugins/bootstrap/css/bootstrap.min.css"></link>
+    <script type="text/javascript" src="../../../plugins/bootstrap/js/jquery-3.3.1.js"></script>
+    <script type="text/javascript" src="../../../plugins/bootstrap/js/bootstrap.min.js"></script>
+    <title>Proyecto SGOA</title>
+</head>
+<style>
+    /* Remove the navbar's default margin-bottom and rounded borders */
+    .navbar {
+        margin-bottom: 0;
+        border-radius: 0;
+    }
+
+    /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
+    .row.content {height: 390px}
+
+    /* Set gray background color and 100% height */
+    .sidenav {
+        padding-top: 20px;
+        background-color: #f1f1f1;
+        height: 100%;
+    }
+
+    html{
+        min-height: 100%;
+        position: relative;
+    }
+    body{
+        margin:0;
+        margin-bottom: 40px;
+    }
+    /* Set black background color, white text and some padding */
+    footer {
+        background-color: #555;
+        color: white;
+        padding: 15px;
+        position: fixed;
+        bottom: 0;
+        width: 100%;
+        padding-top:5px;
+    padding-bottom:5px;
+    }
+
+    /* On small screens, set height to 'auto' for sidenav and grid */
+    @media screen and (max-width: 767px) {
+        .sidenav {
+            height: auto;
+            padding: 15px;
+        }
+        .row.content {height:auto;}
+    }
+
+    .table > tbody > tr > td {
+        vertical-align: middle;
+    }
+
+       .estadistica{
+        -webkit-column-count: 3; /* Chrome, Safari, Opera */
+        -moz-column-count: 3; /* Firefox */
+        column-count: 2;
+        
+    }
+
+</style>
+
+
+<body>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="es">
+<head>
+
+    <meta charset="utf-8"></meta>
+    <link rel="stylesheet" href="../../plugins/bootstrap/css/bootstrap.min.css"></link>
+    <script type="text/javascript" src="../../plugins/bootstrap/js/jquery-3.3.1.js"></script>
+    <script type="text/javascript" src="../../plugins/bootstrap/js/bootstrap.min.js"></script>
+    <title>Proyecto SGOA</title>
+</head>
+<style>
+    /* Remove the navbar's default margin-bottom and rounded borders */
+    .navbar {
+        margin-bottom: 0;
+        border-radius: 0;
+    }
+
+    /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
+    .row.content {height: 390px}
+
+    /* Set gray background color and 100% height */
+    .sidenav {
+        padding-top: 20px;
+        background-color: #f1f1f1;
+        height: 100%;
+    }
+
+    html{
+        min-height: 100%;
+        position: relative;
+    }
+    body{
+        margin:0;
+        margin-bottom: 40px;
+    }
+    /* Set black background color, white text and some padding */
+    footer {
+        background-color: #555;
+        color: white;
+        padding: 15px;
+        position: fixed;
+        bottom: 0;
+        width: 100%;
+        padding-top:5px;
+    padding-bottom:5px;
+    }
+
+    /* On small screens, set height to 'auto' for sidenav and grid */
+    @media screen and (max-width: 767px) {
+        .sidenav {
+            height: auto;
+            padding: 15px;
+        }
+        .row.content {height:auto;}
+    }
+
+    .table > tbody > tr > td {
+        vertical-align: middle;
+    }
+
+       .estadistica{
+        -webkit-column-count: 3; /* Chrome, Safari, Opera */
+        -moz-column-count: 3; /* Firefox */
+        column-count: 2;
+        
+    }
+
+</style>
+
+
+<body>
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Bienvenid@: <strong><?php echo $_SESSION['usuario'] ?></strong></a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+                <li><a href="../modulos_profesor/pro_importar_catalogar.php">Importar y catalogar</a></li>
+                <li><a href="../modulos_profesor/pro_herramientas.php">Herramientas</a></li>
+                <li class="active"><a href="../modulos_comunes/index.php?<?php echo $_SESSION['usuario'] ?>">Foro</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="../desconectar_sesion.php"><span class="glyphicon glyphicon-log-out"></span> Salir</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<!-- Inicio formulario de búsqueda -->
+
+<!-- presentacion de objetos de aprendizaje-->
+<div class="container-fluid text-center">
+    <div class="row content">
+        <!-- --------------------------------------------- -->
+                <div class="col-md-3 text-center">
+                    <!--<input type="text" class="form-control" id="criterio_busqueda" placeholder="Buscar...." name="criterio_busqueda" required></br>-->
+                </div>
+                <div class="col-md-3 text-left">
+                    <br><br>
+                </div>
+
+            </form>
+
+            <div class="container" >
+                <table class="table table-striped" border ="1|1" class="table table-bordered" id="tabla">
+                    <thead>
+                    <tr class="warning">
+                        <td>Usuario</td>
+                        <td>Titulo</td>
+                        <td>Fecha</td>
+                        <td>Número de respuestas</td>
+					</tr>
+					</thead>
+            </div>
 <?php 
 	
 	require '../../clases_negocio/clase_conexion.php';
@@ -14,8 +211,13 @@
 	$nombre = $_GET['nombre'];
 	else $nombre = 'CB';
 
+	$idLogin = $_SESSION['id'];
+	$nombre = $_SESSION['usuario'];
+
 	
-	$conexion = new Conexion();
+
+	
+	/*$conexion = new Conexion();
 	$statement = "select idUsuario from usuario where usuario = '".$nombre."' ";
 	$consulta = $conexion->prepare($statement);
 	$consulta->setFetchMode(PDO::FETCH_ASSOC);
@@ -23,9 +225,9 @@
 
 	while($row = $consulta->fetch()){
 		$idLogin = $row['idUsuario'];
-	}
+	}*/
 
-	
+
 	$conexion = new Conexion();
 	$query = "SELECT * FROM foro join usuario on(foro.idUsuario=usuario.idUsuario) WHERE foro.identificador = 0 ORDER BY fecha DESC";
     $consulta = $conexion->prepare($query);
@@ -43,13 +245,15 @@
 			echo "<td>$titulo</td>";
 			echo "<td>".date("d-m-y,$fecha")."</td>";
 			echo "<td>$respuestas</td>";
-			echo "<td><a href= foro.php?id=".$id."&idLogin=".$idLogin."&nombre=".$nombre.">Revisar tema</a></td>";
+			echo "<td><a href= foro.php?id=".$id.">Revisar tema</a></td>";
 		echo "</tr>";
 	}
 ?>
 </table>
 <br>
 <br>
-<?php echo "<a href=formulario.php?idLogin=".$idLogin."&nombre="."$nombre"."> INSERTAR UN NUEVO TEMA </a>";
+<?php echo "<a href=formulario.php> INSERTAR UN NUEVO TEMA </a>";?>
 
+</body>
+</html>
 
